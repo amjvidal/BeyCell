46db54eada6ac6ee02af0f09047ea738c8eca9da
amjvidal <antoniomariojesus123@gmail.com>
2025-11-15 16:13:47 -0300

    criando as classes produto e itemVenda

---
+++ main/model/ItemVenda.java
@@ -0,0 +1,53 @@
+package main.model;
+
+import java.io.Serializable;
+
+public class ItemVenda implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+
+    private Produto produto;
+    private int quantidade;
+    private double subtotal;
+
+    public ItemVenda(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.subtotal = produto.getPreco() * quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+        atualizarSubtotal();
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+        atualizarSubtotal();
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    private void atualizarSubtotal() {
+        this.subtotal = produto.getPreco() * quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemVenda {" +
+                "produto=" + produto.getNome() +
+                ", quantidade=" + quantidade +
+                ", subtotal=" + subtotal +
+                '}';
+    }
+}
--- main/model/Produto.java
+++ main/model/Produto.java
@@ -24,9 +24,75 @@ public class Produto  implements Serializable{
         this.estoqueMinimo = estoqueMinimo;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
 
 
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
 
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getEstoqueAtual() {
+        return estoqueAtual;
+    }
+
+    public void setEstoqueAtual(int estoqueAtual) {
+        this.estoqueAtual = estoqueAtual;
+    }
+
+    public int getEstoqueMinimo() {
+        return estoqueMinimo;
+    }
+
+    public void setEstoqueMinimo(int estoqueMinimo) {
+        this.estoqueMinimo = estoqueMinimo;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto {" + 
+                "id= " + id +
+                ", nome=' " + nome + '\'' +
+                ", marca=' " + marca + '\'' +
+                ", modelo=' " + modelo + '\'' +
+                ", preço= " + preco + 
+                ", estoqueAtual= " + estoqueAtual + 
+                ", estoqueMinimo= " + estoqueMinimo +
+                '}';
+    }
 
 
 }
